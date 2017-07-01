a5e12cb2ab514052ebaecd3e60e577ecc1aa7fd1
lefjor <lefjor@gmail.com>
2017-07-01 19:50:34 +0200

    Catching error from login and user information

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -2,23 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../service/auth.service';
 
+import {FirebaseError} from 'firebase/app';
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
-  email: string;
-  password: string;
+  email:string;
+  password:string;
 
-  constructor(private authService : AuthService) { }
+  constructor(private authService:AuthService) {
+  }
+
+  error : FirebaseError;
+  isError : boolean = false;
 
   ngOnInit() {
   }
 
-  login() : void {
-    this.authService.loginWithEmailAndPassword(this.email, this.password);
-    this.email = this.password = "";
+  login():void {
+    this.authService.loginWithEmailAndPassword(this.email, this.password).catch((error : FirebaseError) => {
+      this.error = error;
+      this.isError = true;
+    });
+    this.password = "";
   }
 
   logout() {
@@ -26,7 +35,10 @@ export class LoginFormComponent implements OnInit {
   }
 
   signup() {
-    this.authService.signup(this.email, this.password);
+    this.authService.signup(this.email, this.password).catch((error : FirebaseError) => {
+      this.error = error;
+      this.isError = true;
+    });;
     this.email = this.password = '';
   }
 
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -13,30 +13,15 @@ export class AuthService {
     this.authState$ = afAuth.authState;
   }
 
-  loginWithEmailAndPassword(email:string, password:string) {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password).then(res => {
-      console.log(res);
-      console.log("je suis loggué");
-    }).catch(error => {
-      console.log(error);
-      console.log("Mais heuuuu");
-    });
+  loginWithEmailAndPassword(email:string, password:string):firebase.Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
   }
 
-  signup(email: string, password: string) {
-    this.afAuth
-      .auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(value => {
-        console.log('Success!', value);
-      })
-      .catch(err => {
-        console.log('Something went wrong:',err.message);
-      });
+  signup(email:string, password:string):firebase.Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
   }
 
   logout() {
     return this.afAuth.auth.signOut();
   }
-
 }
